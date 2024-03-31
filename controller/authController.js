bb9a3796cc6c531ee4bb7b4ee3301e02dbae8552
bansibavishi <bbansi2008@gmail.com>
2024-03-31 13:02:13 +0530

    ~code

--- controller/authController.js
+++ controller/authController.js
@@ -184,7 +184,7 @@ async function logout(req, res) {
 async function profile(req, res) {
     try {
         // var currentUser = req.session.user;
-        var user = await UserModel.findOne({ _id: req.payload._id }, { 'firstName': 1, 'lastName': 1, 'email': 1, 'mobile': 1, 'expertise': 1, 'language': 1, 'title': 1, 'description': 1, 'workHistory': 1, 'location': 1, 'savedJob': 1, 'rate': 1, 'img': 1 });
+        var user = await UserModel.findOne({ _id: req.payload._id }, { 'type': 1, 'firstName': 1, 'lastName': 1, 'email': 1, 'mobile': 1, 'expertise': 1, 'language': 1, 'title': 1, 'description': 1, 'workHistory': 1, 'location': 1, 'savedJob': 1, 'rate': 1, 'img': 1 });
         var createLink = "http://127.0.0.1:4000" + user.img;
         var reviews = await reviewsModel.find({ userId: req.payload._id });
         var avarageReview = 0;
@@ -269,10 +269,6 @@ async function update(req, res) {
             }
         }
 
-
-
-
-
         var user1 = await UserModel.updateOne({ _id: userId }, data);
 
 
@@ -338,7 +334,7 @@ async function forgetPass(req, res) {
         if (!user)
             return res.status(400).json({
                 message: "user with given email doesn't exist",
-                status : false
+                status: false
             });
 
         let token = await tokenModel.findOne({ userId: user._id });
